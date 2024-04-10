da48dab7cdf10f31f72f5afc7f4c65b59938412e
k8vlar <kevin.vellard.gmail.com>
2024-04-10 10:20:25 +0200

    addForm

---
+++ src/Form.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+
+class Form extends Component{
+    render () {
+        return (
+        <div>
+          <h1>Kevin c la merde</h1>  
+        </div>
+        )
+    } 
+       
+    
+ 
+}
+
+export default Form;
